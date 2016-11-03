c987d67388acc7cf21f72f1715d3affbbc32315a
vucu <cxv@g.ucla.edu>
2016-11-02 23:17:06 -0700

    More unit test

--- MainProject/acc/forms.py
+++ MainProject/acc/forms.py
@@ -5,6 +5,19 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class RegistrationForm(UserCreationForm):
+    """
+            Create a new user. It takes 3 parameters:
+            username
+            password1
+            password2
+
+            Throw errors in these scenario:
+            username already exists in database
+            same username and password
+            username contains invalid characters (such as !@#$%...)
+            password1/password2 too short (<8 characters)
+            password1 is not the same as password2
+    """
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2')
@@ -18,12 +31,21 @@ class RegistrationForm(UserCreationForm):
     username = forms.CharField(label="Username", max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
     password1 = forms.CharField(label="Password", max_length=30,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
     password2 = forms.CharField(label="Repeat Password", max_length=30,
-                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
 
 
 class LoginForm(AuthenticationForm):
+    """
+                Authenticate a user. It takes 2 parameters:
+                username
+                password
+
+                Throw errors in these scenario:
+                username does not exists in database
+                password is wrong
+    """
     username = forms.CharField(label="Username", max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
     password = forms.CharField(label="Password", max_length=30,
--- MainProject/acc/tests.py
+++ MainProject/acc/tests.py
@@ -51,6 +51,36 @@ class AccTest(TestCase):
         form = RegistrationForm(data=formData)
         self.assertFalse(form.is_valid())
 
+    def testRegistrationForm9(self):
+        formData = {'username': 'tooLong123456789012345678901234567890',
+                    'password1': 'aaaaaaaaaa',
+                    'password2': 'aaaaaaaaaa'}
+        form = RegistrationForm(data=formData)
+        self.assertFalse(form.is_valid())
+
+    def testRegistrationForm10(self):
+        formData = {'username': 'normal3',
+                    'password1': 'tooLong123456789012345678901234567890',
+                    'password2': 'tooLong123456789012345678901234567890'}
+        form = RegistrationForm(data=formData)
+        self.assertFalse(form.is_valid())
+
+    def testRegistrationForm11(self):
+        formData = {'username': 'tooLongB123456789012345678901234567890',
+                    'password1': 'tooLongABCABCABCABCABCABCABCABCABCABCABCABC',
+                    'password2': 'tooLongABCABCABCABCABCABCABCABCABCABCABCABC'}
+        form = RegistrationForm(data=formData)
+        self.assertFalse(form.is_valid())
+
+    def testRegistrationForm12(self):
+        for i in range(0,1000):
+            formData = {'username': 'Spam'+str(i),
+                        'password1': 'aaaaaaaaaa',
+                        'password2': 'aaaaaaaaaa'}
+            form = RegistrationForm(data=formData)
+            self.assertTrue(form.is_valid())
+            form.save()
+
     def testLoginForm1(self):
         formData = {'username': 'doesNotExist', 'password': 'aaaaaaaaaa'}
         form = LoginForm(data=formData)
