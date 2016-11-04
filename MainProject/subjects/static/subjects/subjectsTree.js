c86791190bfcdb95c0fb601c55f0922c70af42d6
Paul KIm <paulmkim>
2016-11-03 22:06:47 -0700

    ready for presentation

--- MainProject/subjects/static/subjects/subjectsTree.js
+++ MainProject/subjects/static/subjects/subjectsTree.js
@@ -151,9 +151,9 @@ function initialize() {
     changeSize(d3.select("#currentDisplay").attr("item_value"));
 
     // Open up some of the tree branches.
-    viz.toggleNode(data.values[2]);
-    viz.toggleNode(data.values[2].values[0]);
-    viz.toggleNode(data.values[3]);
+//    viz.toggleNode(data.values[2]);
+//    viz.toggleNode(data.values[2].values[0]);
+//    viz.toggleNode(data.values[3]);
 
 }
 
