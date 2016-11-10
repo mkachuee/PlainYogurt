d74134ba51aacf6f70583cb71ed1e1faea3597f3
vucu <cxv@g.ucla.edu>
2016-11-09 21:59:16 -0800

    Add query test

--- MainProject/search/views.py
+++ MainProject/search/views.py
@@ -4,24 +4,28 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.template.context_processors import csrf
 from dbaccess import search_tree
+from dbaccess import get_query
 
 from django.http import HttpResponse
 from django.template import loader
 
+
 # Create your views here.
 def search(request):
-	title = "Search Results"
+    title = "Search Results"
 
-	query = {}
-	query['title'] = "Query"
-	search = "Machine Learning"
-	query['search'] = search
-	result = search_tree(search)
-	query['result'] = result
+    query = {}
+    query['title'] = "Query"
+    search = "Machine Learning"
+    query['search'] = search
+    result = search_tree(search)
+    query['result'] = result
+    queryObject = get_query(search, 'name')
+    query['object'] = queryObject
 
-	context = {}
-	context.update(csrf(request))
-	context['title']=title;
-	context['query']=query;
+    context = {}
+    context.update(csrf(request))
+    context['title'] = title;
+    context['query'] = query;
 
-	return render(request, 'search/search.html', context)
+    return render(request, 'search/search.html', context)
