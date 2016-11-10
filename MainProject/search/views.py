d3db3a7409f7f0c65ab62186b5bc1c587c18158f
vucu <cxv@g.ucla.edu>
2016-11-09 21:49:34 -0800

    Add DB query

--- MainProject/search/views.py
+++ MainProject/search/views.py
@@ -2,10 +2,26 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.template.context_processors import csrf
+from dbaccess import search_tree
 
 from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
 def search(request):
-	return render(request, 'search/search.html')
+	title = "Search Results"
+
+	query = {}
+	query['title'] = "Query"
+	search = "Machine Learning"
+	query['search'] = search
+	result = search_tree(search)
+	query['result'] = result
+
+	context = {}
+	context.update(csrf(request))
+	context['title']=title;
+	context['query']=query;
+
+	return render(request, 'search/search.html', context)
