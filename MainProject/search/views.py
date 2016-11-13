78704d9074db2635c5c932276878278fc24021c3
vucu <cxv@g.ucla.edu>
2016-11-13 13:19:30 -0800

    More detailed template

--- MainProject/search/views.py
+++ MainProject/search/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.template.context_processors import csrf
 from dbaccess import search_tree
 from dbaccess import get_query
+from tree_db_interface import search_trees, load_trees
 
 from django.http import HttpResponse
 from django.template import loader
@@ -15,26 +16,34 @@ def search(request):
     q = ""
     if request.method == 'GET':
         q = request.GET.get('q','')
-    title = "Search"
 
     query = {}
     query['title'] = "Query:" + q
-    search = "Machine Learning"
-    query['search'] = q
+
     if q:
-        result = search_tree(q)
+        querySetResult = search_trees(q)
+        result = load_trees(querySetResult)
     else:
         result = ""
     query['result'] = result
+
     if q:
         queryObject = get_query(q, 'name')
     else:
         queryObject = ""
     query['object'] = queryObject
 
+    query['count1'] = range(0, 3)
+    query['count2'] = range(0, 4)
+    querySetResultTable = {}
+
+
     context = {}
     context.update(csrf(request))
-    context['title'] = title;
-    context['query'] = query;
+    context['query'] = query
+    context['result'] = result
+    context['tuple'] = querySetResult
+    context['firstTuple'] = querySetResult[0]
+    context['firstResult'] = result[0]
 
     return render(request, 'search/search.html', context)
