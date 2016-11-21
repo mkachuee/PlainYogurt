fc658abf05b37e5698ce696e4159123a186c1a17
paulmkim <paul.m.kim321@gmail.com>
2016-11-20 19:02:55 -0800

    changed data format

--- MainProject/subjects/views.py
+++ MainProject/subjects/views.py
@@ -18,10 +18,19 @@ def allSubjects(request):
 def specificSubject(request, subjectName):
 	# get subjectName from database and put into context
 	module_dir = os.path.dirname(__file__)  # get current directory
-	data_path = os.path.join(module_dir, 'tree_list.pkl')
+	
+	# TODO: remove the next 3 lines and replace data with the {path,content} dictionary for the correct subject
+	data_path = os.path.join(module_dir, 'example_cnn.pkl')
 	with open(data_path, 'rb') as f:
 		data = pickle.load(f)
 
+	
+	data['contents'][0] = {'name':'name 1', 'description':'des1', 'links':[{link:'//http',name:'wiki'}]}
+	
+
+	for key,value in data['contents'].items():
+		data['contents'][key] = value.replace('\n','');
+
 	data_in_json = json.dumps(data)
-	context = {'subjectName': subjectName, 'data': data_in_json}
+	context = {'subjectName': subjectName, 'data': data_in_json, 'id_name_pair': id_name_pair}
 	return render(request, 'subjects/specificSubject.html', context)
