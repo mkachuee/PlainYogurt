8bc48e2f3b5e68044a4c4473c3376c9c02b8fdfe
vucu <cxv@g.ucla.edu>
2016-11-20 20:43:34 -0800

    Add login success view

--- MainProject/acc/urls.py
+++ MainProject/acc/urls.py
@@ -5,8 +5,9 @@ from acc import forms
 
 urlpatterns = [
     url(r'^$', accView.home, name='home'),
-    url(r'^login/$', authView.login, {'template_name': 'acc/login.html', 'authentication_form': forms.LoginForm},
+    url(r'^login/$', accView.customLogin, {'template_name': 'acc/login.html', 'authentication_form': forms.LoginForm},
         name='login'),
+    url(r'^loginSuccess/$', accView.loginSuccess, name='loginSuccess'),
     url(r'^logout/$', authView.logout, {'next_page': 'login'}),
     url(r'^register/$', accView.register, name='register'),
     url(r'^registerSuccess/$', accView.registerSuccess, name='registerSuccess')
--- MainProject/acc/views.py
+++ MainProject/acc/views.py
@@ -5,6 +5,36 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.template.context_processors import csrf
 from acc.forms import RegistrationForm
+from django.contrib.auth.views import login
+
+import functools
+import warnings
+
+from django.conf import settings
+# Avoid shadowing the login() and logout() views below.
+from django.contrib.auth import (
+    REDIRECT_FIELD_NAME, get_user_model, login as auth_login,
+    logout as auth_logout, update_session_auth_hash,
+)
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import (
+    AuthenticationForm, PasswordChangeForm, PasswordResetForm, SetPasswordForm,
+)
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponseRedirect, QueryDict
+from django.shortcuts import resolve_url
+from django.template.response import TemplateResponse
+from django.urls import reverse
+from django.utils.deprecation import RemovedInDjango20Warning
+from django.utils.encoding import force_text
+from django.utils.http import is_safe_url, urlsafe_base64_decode
+from django.utils.six.moves.urllib.parse import urlparse, urlunparse
+from django.utils.translation import ugettext as _
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+
 
 
 def register(request):
@@ -26,6 +56,21 @@ def register(request):
 def registerSuccess(request):
     return render_to_response('acc/registerSuccess.html')
 
+
+def customLogin(request, template_name='registration/login.html',
+          redirect_field_name=REDIRECT_FIELD_NAME,
+          authentication_form=AuthenticationForm,
+          extra_context=None, redirect_authenticated_user=False):
+        return login(request,
+                 template_name,
+                 redirect_field_name,
+                 authentication_form,
+                 extra_context,
+                 redirect_authenticated_user)
+
+
+def loginSuccess(request):
+    return render_to_response('acc/home.html')
 
 @login_required(login_url="login/")
 def home(request):
