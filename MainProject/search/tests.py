3b5e9a0cef99625ce3aac15431e90228b577bf31
vucu <cxv@g.ucla.edu>
2016-11-27 21:40:43 -0800

    Test for search

--- MainProject/search/tests.py
+++ MainProject/search/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+class SearchTest(TestCase):
+    def testSearching(self):
+        response = self.client.get('/search/', {'q': 'Algebra'})
+        print(response.status_code)
+        self.assertEqual(response.status_code, response.status_code)
+
+    def testSearchingBlank(self):
+        response = self.client.get('/search/', {'q': ''})
+        print(response.status_code)
+        self.assertEqual(response.status_code, response.status_code)
